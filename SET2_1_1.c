36a3d375759a6046c3187e55cc1fbe580da668b9
ramyavenkatachalam <47887238+ramyavenkatachalam@users.noreply.github.com>
2019-02-22 16:31:30 +0530

    Create SET2_1_1.c

---
+++ SET2_1_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int b,n,k;
+    printf("enter the value n,k");
+    scanf("%d%d",&n,&k);
+    b=pow(n,k);
+    printf("%d",b);
+    return 0;
+}
