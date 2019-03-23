c01919208556e136098a933dc727dd5ebf79d89a
ramyavenkatachalam <47887238+ramyavenkatachalam@users.noreply.github.com>
2019-03-23 17:59:23 +0530

    Create b__2_7.c

---
+++ b__2_7.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int n=0;num,sum=0,rem=0,count=0,n1;
+scanf("%d",&num);
+n1=n=num;
+while(n!=0)
+{
+count++;
+n=n/10;
+}
+while(n1!=0)
+{
+rem=n1%10;
+sum=sum+pow(rem,count);
+n1=n1/10;
+}
+if(sum==num)
+{
+printf("armstrong");
+}
+else
+{
+printf("not armstrong");
+}
+return 0;
+}
