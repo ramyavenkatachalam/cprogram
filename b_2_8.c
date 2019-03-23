9604797431714f29e9680337c4cc3c0dc5aceabd
ramyavenkatachalam <47887238+ramyavenkatachalam@users.noreply.github.com>
2019-03-23 18:09:30 +0530

    Create b_2_8.c

---
+++ b_2_8.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int n1,n2,sum=0,count=0,temp=0,temp1=0,i,rem=0;
+scanf("%d%d",&n1,&n2);
+for(i=n1;i<n2;i++)
+{
+temp=i;
+temp1=i;
+while(temp!=0)
+{
+count++;
+temp=temp/10;
+}
+while(temp1!=0)
+{
+rem=temp1%10;
+sum=sum+pow(rem,count);
+temp1=temp1/10;
+}
+if(sum==i)
+{
+printf("%d",sum);
+}
+sum=0;
+count=0;
+}
+return 0;
+}
